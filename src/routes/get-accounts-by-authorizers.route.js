faca8d3339a266e4ae63012d87d8eb146225ea2c
Adriel Diaz <adriexnet@gmail.com>
2020-11-02 11:27:53 -0600

    fix: empty account using application/x-www-form-urlencoded content-type

---
+++ src/routes/get-accounts-by-authorizers.route.js
@@ -0,0 +1,34 @@
+const axios = require('axios').default
+var qs = require('qs')
+
+const { eosConfig } = require('../config')
+const { errorUtil } = require('../utils')
+
+module.exports = {
+  method: 'POST',
+  path: '/v1/chain/get_accounts_by_authorizers',
+  handler: async (req, h) => {
+    try {
+      console.log('get_accounts_by_authorizers', 'middleware')
+      let payload = req.payload || {}
+
+      if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+        payload = {}
+        Object.keys(req.payload).forEach((element) => {
+          payload = { ...payload, ...JSON.parse(element) }
+        })
+      }
+
+      const { data } = await axios.post(
+        `${eosConfig.apiEndpoint}/v1/chain/get_accounts_by_authorizers`,
+        payload
+      )
+
+      return data
+    } catch (error) {
+      const standardError = errorUtil.getStandardError(error)
+
+      return h.response(standardError).code(standardError.code)
+    }
+  }
+}
