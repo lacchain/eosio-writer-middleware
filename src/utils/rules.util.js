d954ce7f0179cac9ebe4f4db3da93bb3c04aa6dc
Adriel Diaz <adriexnet@gmail.com>
2020-11-26 15:48:48 -0600

    feat: add rule validation for account name

--- src/utils/rules.util.js
+++ src/utils/rules.util.js
@@ -1,3 +1,4 @@
+const { eosConfig } = require('../config')
 const { ValidationRuleError } = require('./error.util')
 
 const validateTransction = (transaction) => {
@@ -21,7 +22,7 @@ const validateTransction = (transaction) => {
     })
   }
 
-  // first action must be from one writter and execute run action
+  // first action must be from writter account with run action
   const firstAction = transaction.actions[0]
   if (firstAction.account !== 'writer' || firstAction.name !== 'run') {
     throw new ValidationRuleError({
@@ -30,10 +31,31 @@ const validateTransction = (transaction) => {
       error: {
         code: 515193183,
         name: 'invalid_writer_action_exception',
-        what: 'Writer action "run" must be provided at beginning of the transaction',
+        what:
+          'Writer action "run" must be provided at beginning of the transaction',
         details: [
           {
-            message: 'Writer action "run" is required and must be the first action in the transaction'
+            message:
+              'Writer action "run" is required and must be the first action in the transaction'
+          }
+        ]
+      }
+    })
+  }
+
+  // first authorization must be the accountName set for this instance
+  const firstAuthorization = firstAction.authorization[0]
+  if (firstAuthorization.actor !== eosConfig.writer.accountName) {
+    throw new ValidationRuleError({
+      code: 500,
+      message: 'Internal Service Error',
+      error: {
+        code: 515193184,
+        name: 'invalid_actor_action_exception',
+        what: 'Invalid actor for "run" action',
+        details: [
+          {
+            message: `Actor for "run" action must be ${eosConfig.writer.accountName}`
           }
         ]
       }
